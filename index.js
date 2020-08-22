f840ea4fc4224018d2cc0fb3c866e032469eca58
Sohaib Ilyas <contact@sohaibilyas.com>
2020-08-23 04:03:56 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const axios = require("axios");
+
+module.exports = class Facebook {
+  config = {};
+
+  accessToken = null;
+  baseUrl = "https://graph.facebook.com/";
+
+  constructor(config) {
+    if (!config.graphVersion) {
+      config.graphVersion = "v7.0";
+    }
+
+    this.config = Object.assign({}, config, this.config);
+    this.baseUrl += this.config.graphVersion;
+  }
+
+  getLoginUrl(permissions = []) {
+    const state =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
+
+    return (
+      "https://www.facebook.com/" +
+      this.config.graphVersion +
+      "/dialog/oauth?client_id=" +
+      this.config.appId +
+      "&redirect_uri=" +
+      this.config.redirectUrl +
+      "&scope=" +
+      permissions.join(",") +
+      "&state=" +
+      state
+    );
+  }
+
+  callback(code) {
+    axios
+      .get(
+        this.baseUrl +
+          "/oauth/access_token?client_id=" +
+          this.config.appId +
+          "&client_secret=" +
+          this.config.appSecret +
+          "&redirect_uri=" +
+          this.config.redirectUrl +
+          "&code=" +
+          code
+      )
+      .then((response) => {
+        this.accessToken = response.data.access_token;
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }
+
+  getAccessToken() {
+    return this.accessToken;
+  }
+
+  getBaseUrl() {
+    return this.baseUrl;
+  }
+
+  setAccessToken(accessToken) {
+    this.accessToken = accessToken;
+  }
+
+  get(path, accessToken) {
+    if (!accessToken) {
+      accessToken = this.accessToken;
+    }
+
+    var separator = "?";
+    if (path.includes("?")) {
+      separator = "&";
+    }
+
+    return axios.get(
+      this.baseUrl + path + separator + "access_token=" + accessToken
+    );
+  }
+};
